c51402a0db8f4c9701466f91818f1459bd56bf5b
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-01-29 08:59:11 -0300

    adding environment variables

--- web/src/services/socket.ts
+++ web/src/services/socket.ts
@@ -2,7 +2,10 @@ import io from 'socket.io-client';
 
 type EventCallback = (data: any) => void;
 
-const socket = io('http://localhost:3333');
+const HOST = import.meta.env.VITE_SERVER_HOST || 'localhost';
+const PORT = import.meta.env.VITE_SERVER_PORT || 3333;
+
+const socket = io(`http://${HOST}:${PORT}`);
 
 function configureBroadcastEvent(callback: EventCallback) {
   socket.on('broadcast', data => callback(data));
